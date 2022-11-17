2f458a2d642c93a9082ee65e7052dce23a1d4e43
Benjamin Kravets <bakravets@gmail.com>
2022-11-16 20:40:48 -0500

    add goober

---
+++ goober.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env
+import sys
+import getopt
+from scapy.all import *
+from ipaddress import IPv4Address
+import sys
+import getopt
+remote_ip = None
+command = None
+time_out = None
+  
+def full_name():
+    first_name = None
+    last_name = None
+  
+    argv = sys.argv[1:]
+    global remote_ip
+    global command
+    global time_out
+  
+    try:
+        opts, args = getopt.getopt(argv, "i:c:t:", 
+                                   ["remote_ip =",
+                                    "command =",
+                                    "timeout ="])
+        for opt, arg in opts:
+        
+            if opt in ['-i', '--remote_ip']:
+                remote_ip = IPv4Address(arg)
+            if opt in ['-c', '--command']:
+                command = arg
+            elif opt in ['-t', '--timeout']:
+                time_out = int(arg)
+        
+  
+        
+    except Exception as e: print(e)
+        
+
+        #print("Remote IP (-i) and command (-c) are required")
+  
+
+
+def send_command():
+        
+        pack = srp1(Ether()/IP(dst=str(remote_ip))/(ICMP()/command),timeout=2)
+        #print(command)
+        try:
+        #pack.show()
+                sys.exit()
+        except Exception as e:
+                print(e)
+                print("no reply")
+                sys.exit()
+	
+full_name()
+send_command()
+
